0d7486bda11d6d7ad45d79e9f10a25c5bca24732
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-01-30 18:19:23 +0500

    Limitlar

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 
 using BookApplication;
 using BookInfrastructure;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+using System.Timers;
 
 namespace BookCatalogApi
 {
@@ -25,10 +28,68 @@ namespace BookCatalogApi
             builder.Services.AddResponseCaching();
             builder.Services.AddOutputCache();
             builder.Services.AddControllers();
+
+
+            builder.Services.AddRateLimiter(options =>
+            {
+                options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+                options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+                RateLimitPartition.GetSlidingWindowLimiter(
+                    partitionKey: "FixedWindowLimiter",
+                    factory: x => new SlidingWindowRateLimiterOptions
+                    {
+                        PermitLimit = 8,
+                        Window = TimeSpan.FromSeconds(20),
+                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                        QueueLimit = 0,
+                        AutoReplenishment = true,
+                        SegmentsPerWindow = 3
+                    }));
+            });
+
+
+
+            /*builder.Services.AddRateLimiter(options =>
+            {
+                options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+                options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+                RateLimitPartition.GetFixedWindowLimiter(
+                    partitionKey: "FixedWindowLimiter",
+                    factory: x => new FixedWindowRateLimiterOptions
+                    {
+                        PermitLimit = 4,
+                        Window = TimeSpan.FromSeconds(10),
+                        QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                        QueueLimit = 0,
+                        AutoReplenishment = true
+                    }));
+            });*/
+
+
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 1000;
+            timer.Start();
+
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            app.Use((context, next) =>
+            {
+                Console.WriteLine("Requst coming .....");
+                return next(context);
+            });
+
+            app.UseRateLimiter();
+
+           
+
 
             if (app.Environment.IsDevelopment())
             {
@@ -45,6 +106,12 @@ namespace BookCatalogApi
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine(e.SignalTime);
         }
     }
 }
