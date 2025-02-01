51928b586e59fa2b1255c26a9eb3a6ad6c356e52
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-02-01 17:28:57 +0500

    update va get qo'shildi

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using BookApplication.Extensions;
 using BookApplication.Models;
 using BookApplication.Repositories;
+using BookCatalogApiDomain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookCatalogApi.Controllers;
@@ -31,7 +32,35 @@ public class AccountController : ControllerBase
             return Ok(_tokenService.CreateToken());
         }
         return BadRequest("Login pr passwrod is Incorrect ......!");
-
-
     }
+    [HttpPost]
+    [Route("Create")]
+    public async Task<IActionResult> Create([FromBody] User user)
+    {
+        if (ModelState.IsValid)
+        {
+            if (await _userRepository.AddAsync(user) != null)
+            {
+                return Ok();
+            }
+        }
+        return BadRequest();
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        IQueryable<User> res = await _userRepository.GetAsync(x => true);
+        return Ok(res);
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateUser([FromBody] User user)
+    {
+        if(ModelState.IsValid)
+        {
+            user = await _userRepository.UpdateAsync(user);
+            return Ok(user);
+        }
+        return BadRequest();
+    }
+
 }
